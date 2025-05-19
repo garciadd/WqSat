0b96a9b5d71d353be171eef882824a1106023a2c
garciadd <garciad@ifca.unican.es>
2025-05-19 13:02:58 +0200

    cleaning

--- wq_sat/utils/main_utils.py
+++
@@ -1,8 +0,0 @@
-
-def read_download_file(download):
-    # Abre el archivo en modo lectura
-    with open(download, 'r') as file:
-        # Lee todo el contenido del archivo
-        contenido = file.read()
-
-    print(contenido)
